0bb27e20e4cfaca27c95c5aa039220c26a173b9c
SumetPH <notsumet1@gmail.com>
2020-07-04 09:36:07 +0700

    edit port

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-http.listen(9000, () => console.log("http://localhost:9000"));
+const port = process.env.PORT || 9000
+http.listen(port, () => console.log(`http://localhost:${port}`));
 
 const Twitter = require("twitter");
 const client = new Twitter({
