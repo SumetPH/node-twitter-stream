09105759afe18ef74318320a6753c7655fbcb49d
sumetph <notsumet1@gmail.com>
2020-08-14 21:42:01 +0700

    add bootstrap

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 9000;
 http.listen(port, () => console.log(`http://localhost:${port}`));
 
 const Twitter = require("twitter");
@@ -11,8 +11,10 @@ const client = new Twitter({
   consumer_key: process.env.consumer_key,
   consumer_secret: process.env.consumer_secret,
   access_token_key: process.env.access_token_key,
-  access_token_secret: process.env.access_token_secret,
+  access_token_secret: process.env.access_token_secret
 });
+
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -21,18 +23,18 @@ app.get("/", (req, res) => {
 var tw;
 
 const stream = (socket, track) =>
-  client.stream("statuses/filter", { track: track }, (stream) => {
-    stream.on("data", (event) => {
+  client.stream("statuses/filter", { track: track }, stream => {
+    stream.on("data", event => {
       socket.emit("tweet", event);
     });
 
-    stream.on("error", (error) => {
+    stream.on("error", error => {
       console.log(error);
     });
     tw = stream;
   });
 
-io.on("connection", (socket) => {
+io.on("connection", socket => {
   console.log("a user connected");
   stream(socket, "twice");
 
@@ -40,7 +42,7 @@ io.on("connection", (socket) => {
     tw.destroy();
   });
 
-  socket.on("track", (track) => {
+  socket.on("track", track => {
     tw.destroy();
     stream(socket, track);
   });
